736b3c8c6c1596abd763a8e529b98bdb3c21c0fd
Oleksandr Shkabbura <shkabura007@gmail.com>
2026-06-08 21:50:45 +0300

    fix flake8 error

--- app/main.py
+++ app/main.py
@@ -12,15 +12,18 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-
     def calculate_washing_price(self, car: Car) -> float:
-        washing_price = car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating / self.distance_from_city_center
+        washing_price = (
+                car.comfort_class
+                * (self.clean_power - car.clean_mark)
+                * self.average_rating
+                / self.distance_from_city_center
+        )
         return round(washing_price, 1)
 
     def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
-
 
     def rate_service(self, new_rating: float) -> None:
         sum_before = self.average_rating * self.count_of_ratings
@@ -28,7 +31,6 @@ class CarWashStation:
         sum_new = sum_before + new_rating
         self.average_rating = round(sum_new / new_count, 1)
         self.count_of_ratings = new_count
-
 
     def serve_cars(self, cars: list) -> float:
         total_earnings = 0
